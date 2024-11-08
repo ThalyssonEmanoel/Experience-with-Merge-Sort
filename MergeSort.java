2aec49cafcb258e473c7495283746b181750ee62
Thalysson <thalysson140105@gmail.com>
2024-11-08 15:10:26 -0400

    Feito a função merge

--- MergeSort.java
+++ MergeSort.java
@@ -3,14 +3,15 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static void main(String[] args) {
-        int[] arrayToSort = {3, 2, 56, 7, 1, 12};
+        int[] arrayToSort = {19,18,26,17,20,10,30,31};
         int[] sortedArray = mergeSort(arrayToSort);
         System.out.println(Arrays.toString(sortedArray));
-
     }
 
     public static int[] mergeSort(int[] array) {
-        if (array.length <= 1) return array;
+        if (array.length <= 1) {
+            return array;
+        }
 
         int middle = array.length / 2;
         int[] left = Arrays.copyOfRange(array, 0, middle);
@@ -21,6 +22,23 @@ public class MergeSort {
 
     public static int[] merge(int[] left, int[] right) {
         int[] result = new int[left.length + right.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result[k++] = left[i++];
+            } else {
+                result[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            result[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            result[k++] = right[j++];
+        }
 
         return result;
     }
